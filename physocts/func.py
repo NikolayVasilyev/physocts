ec5793ff1d780b87ae7784ea95c38cfe400e113b
Nikolay S. Vasil'ev <vasilev@kribrum.ru>
2022-08-24 19:36:19 +0300

    ++

--- physocts/func.py
+++ physocts/func.py
@@ -8,13 +8,17 @@ from typing import Callable, Any, TypeVar
 from functools import partial, reduce
 from operator import is_not
 
+
 X = TypeVar("X")
 Y = TypeVar("Y")
+
 
 def relax(*a, **k):
     del a, k
 
+
 ident = lambda x: x
+
 
 def apply_item(hlr: Callable[[X, Y], None], inst: X, itm: Y):
     """
@@ -33,15 +37,19 @@ flip = lambda f: lambda x, y: f(y, x)
 composite: Callable[..., Callable[[Any], Any]]
 composite = lambda *fs: reduce( lambda g, f: lambda x: f(g(x)), fs[::-1], lambda x: x )
 
+
 monad_do_maybe: Callable[..., Callable[[Any], Any]]
 monad_do_maybe = lambda *fs: reduce(
     lambda g, f: lambda x: (lambda res: f(res) if not res is None else None)(g(x)),
     fs[::-1],
     lambda x: x)
 
+
 ident = lambda x: x
 
+
 is_not_none = partial(flip(is_not), None)
+
 
 def test_monad_do_maybe():
 
